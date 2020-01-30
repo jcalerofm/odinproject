498ac960a12ac85d000b63ae4384c6af4d603c9d
Jorge CALERO <jorge.calero@gmail.com>
2020-01-30 16:21:10 +0100

    Add files via upload

---
+++ caesar_cipher.rb
@@ -0,0 +1,7 @@
+def caesar_cipher(string, shift = 4)
+  alphabet   = Array('a'..'z')
+  encrypter  = Hash[alphabet.zip(alphabet.rotate(shift))]
+  string.chars.map { |c| encrypter.fetch(c, " ") }
+end
+
+p caesar_cipher("testing").join
